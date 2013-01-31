b9d7c4dc5f07ebd07a76c198829482df6015e3fd
gekkegast84 <mick.rijneveld@live.nl>
2013-01-31 13:14:34 +0100

    eerste begin

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1, maximum-scale=1">
+	<link rel="shortcut icon" href="http://www.rit.edu/fa/cpd/sites/all/themes/cpd/images/logo_nav.gif">
+	<link rel="stylesheet" type="text/css" href="css/style.css" />	
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+	<link href='http://fonts.googleapis.com/css?family=Bitter:400,700,400italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400italic,400,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+	<title>Mick Rijneveld | School Opdracht</title>    	
+</head>
+	<body>
+		<div id='sidebar'>
+
+		<ul>
+			<li></li>
+		</ul>
+		
+		</div>
+	</body>
+</html>
+	
