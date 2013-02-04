5d68d14349f01a724cf2b621e81947c5ce58b2ae
gekkegast84 <mick.rijneveld@live.nl>
2013-02-04 12:18:02 +0100

    mvc model

---
+++ oldsite/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1, maximum-scale=1">
+	<link rel="shortcut icon" href="http://www.rit.edu/fa/cpd/sites/all/themes/cpd/images/logo_nav.gif">
+	<link rel="stylesheet" type="text/css" href="css/style.css" />	
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400italic,400,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+	<title>Mick Rijneveld | School Opdracht</title>    	
+</head>
+	<body>
+	<?php session_start(); ?>
+		<div id='sidebar'>
+			<a href='index.php?content=pagina/home' ><img src='img/logo.png'/></a>
+			<div id='menu'>
+				<?php include("link.php"); ?>
+			</div>
+		</div>
+			<div id='container'>
+				<div id='content'>
+					<?php include("navigatie.php"); ?>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+	
---
+++ oldsite/link.php
@@ -0,0 +1,5 @@
+<ul>
+	<a href='index.php?content=pagina/home'><li>Home</li></a>
+	<a href='index.php?content=pagina/login'><li>Login</li></a>
+	<a href='index.php?content=pagina/register'><li>Registreren</li></a>
+</ul>
